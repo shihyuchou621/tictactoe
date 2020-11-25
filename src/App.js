dda06ad616151a656dc5b180684b818acccb9eb6
shihyuchou <kellyu621@gmail.com>
2020-11-25 15:06:51 +0800

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const gameOver = [
+  // 直
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  // 橫
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  // 斜
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
-export default App;
+export default function App() {
+  const [player, setPlayer] = useState("O");
+  const [plate, setPlate] = useState([...Array(9)].map(unit => ""));
+  const [over, setOver] = useState(false);
+
+  
+  const handleClick = index => {
+    
+    setPlate([
+      ...plate.slice(0, index),
+      player,
+      ...plate.slice(index + 1),
+    ]);
+    setPlayer(player === "O" ? "X" : "O");
+
+    const answerO = [...plate].map((unit, index) => unit === "O" ? index : ""); 
+    const answerX = [...plate].map((unit, index) => unit === "X" ? index : "");
+    
+    for(let i = 0; i++; i < 9) {
+      console.log(i);
+      if(answerO.includes(gameOver[i][0]) && 
+          answerO.includes(gameOver[i][1]) && 
+            answerO.includes(gameOver[i][2]) 
+      ) {
+        setOver(true);
+      }
+    }
+    
+    console.log(answerO, answerX);
+  }
+
+  const resetGame = () => {
+    setOver(false);
+    setPlate([...Array(9)].map(unit => ""));
+  }
+
+  return (
+    <div>
+      <div className="plate">
+        {
+          plate.map((unit, index) =>
+            <div className="unit" key={index} onClick={handleClick.bind(this, index)}>
+              {`${plate[index]}(${index})`}
+            </div>
+          )
+        }
+      </div>
+      <div>
+        { !!over&&
+          <div>
+          winner: {player}
+          <button onClick={resetGame}>reset</button>
+          </div>
+        }
+      </div>
+    </div>
+  )
+}
